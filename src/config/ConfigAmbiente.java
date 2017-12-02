54a423e55e4f7f3b1370af8a40793c31fbafb3f0
PC <PC@DESKTOP-VR55A0H.Home>
2017-12-02 11:37:02 -0200

    Melhoras no Codigo

--- src/config/ConfigAmbiente.java
+++ src/config/ConfigAmbiente.java
@@ -6,10 +6,11 @@ import javax.swing.JOptionPane;
 
 class ConfigAmbiente {
 	
+	private static String ambienteTrabalho;
+	private static String executavelJAR;
 	private static String nomeNavegador;
 	private static String browserDriver;
 	private static String driver;
-	private static String ambienteTrabalho;
 	private static int confirma;
 	//private static int indice;
 	
@@ -21,7 +22,7 @@ class ConfigAmbiente {
 		
 		if (!config.getConfig()) {
 
-			configurarSelenium();
+			configurar();
 		
 		}
 		
@@ -29,13 +30,13 @@ class ConfigAmbiente {
 	
 	
 	
-	public void configurarSelenium() {
+	public void configurar() {
 		
 		this.browserDriver = null;
 		this.driver = null;
 		this.ambienteTrabalho = null;
 		
-		
+		// Configurar Pasta do Ambiente de Trabalho
 		
 		this.confirma = JOptionPane.showConfirmDialog(null, "Deseja Configurar ou Atualizar a Pasta do Ambiente de Trabalho?", "Configurações do Sistema", JOptionPane.OK_CANCEL_OPTION);
 		
@@ -44,14 +45,89 @@ class ConfigAmbiente {
 			configurarAmbiente();
 			
 		}
-
-
 		
+		// Configurar JAR Executavel
+		
+		this.confirma = JOptionPane.showConfirmDialog(null, "Deseja Configurar ou Atualizar o Executavel JAR?", "Configurações do Sistema", JOptionPane.OK_CANCEL_OPTION);
+		
+		if (this.confirma != JOptionPane.CANCEL_OPTION) {
+
+			configurarExecutavel();
+		
+		}
+
+
+		// Configurar Navegadores
 		
 		
 		this.confirma = JOptionPane.showConfirmDialog(null, "Deseja Cadastrar ou Atializar os Navegadores?", "Configurações do Sistema", JOptionPane.OK_CANCEL_OPTION);
 		
 		if (this.confirma != JOptionPane.CANCEL_OPTION) {
+			configurarNavegadores();
+			
+	}
+	
+}
+	
+	public void configurarAmbiente () {
+		JFileChooser chooser = new JFileChooser(); 
+	    chooser.setCurrentDirectory(new java.io.File("."));
+	    chooser.setDialogTitle("Selecione o Diretorio Ambiente de Trabalho");
+	    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	    chooser.setAcceptAllFileFilterUsed(false);
+
+	    if (chooser.showOpenDialog(chooser) == JFileChooser.APPROVE_OPTION) {
+
+	    	ambienteTrabalho =  String.valueOf(chooser.getSelectedFile())+"\\";
+
+	    	
+			this.confirma = JOptionPane.showConfirmDialog(null, "Confirma as configurações de Ambiente de Trabalho?\n\nDiretório do Ambiente de Trabalho:\n"+this.ambienteTrabalho+"\n\nCaso seja necessário configurar novamente, delete a pasta 'config'.", "Confimação das Configurações do Ambiente de Trabalho", JOptionPane.OK_CANCEL_OPTION);
+			
+			if (this.confirma == JOptionPane.CANCEL_OPTION) {
+
+				JOptionPane.showMessageDialog(null, "Nao foi configurado!", "Configuração do Sistema", JOptionPane.ERROR_MESSAGE);
+				
+			}else {
+
+
+		    	//SalvaConfig gravarConfiguracao = new SalvaConfig(this.browserDriver, this.driver, this.ambienteTrabalho, this.indice);
+			
+			}
+
+
+	      }
+	    else {
+			JOptionPane.showMessageDialog(null, "O Ambiente de Trabalho não foi configurado corretamente!\nO sistema será finalizado devido a falta de configuração.", "Configuração do Ambiente de Trabalho", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+	      }
+	}
+	
+	
+	public void configurarExecutavel(){
+		
+		JFileChooser selecionarArquivo = new JFileChooser();
+		selecionarArquivo.setDialogTitle("Selecione o JAR Executavel do Selenium");
+		int Selecionado = selecionarArquivo.showOpenDialog(null);
+		
+		if (Selecionado == JFileChooser.APPROVE_OPTION) {
+			
+			this.executavelJAR = selecionarArquivo.getSelectedFile().getAbsolutePath();
+		
+		}
+		
+		this.confirma = JOptionPane.showConfirmDialog(null, "Confirma as configurações de Ambiente de Trabalho?\n\nDiretório do Ambiente de Trabalho:\n"+this.ambienteTrabalho+"\n\nCaso seja necessário configurar novamente, delete a pasta 'config'.", "Confimação das Configurações do Ambiente de Trabalho", JOptionPane.OK_CANCEL_OPTION);
+		
+		if (this.confirma == JOptionPane.CANCEL_OPTION) {
+
+			JOptionPane.showMessageDialog(null, "O Selenium nao foi configurado corretamente!\nO sistema será finalizado devido a falta de configuração.", "Configuração do Selenium", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+			
+		}
+
+		
+	}
+	
+	public void configurarNavegadores() {
 
 
 		
@@ -105,43 +181,9 @@ class ConfigAmbiente {
 		
 		}
 		
+
 	}
 	
-}
-	
-	public void configurarAmbiente () {
-		JFileChooser chooser = new JFileChooser(); 
-	    chooser.setCurrentDirectory(new java.io.File("."));
-	    chooser.setDialogTitle("Selecione o Diretorio Ambiente de Trabalho");
-	    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-	    chooser.setAcceptAllFileFilterUsed(false);
-
-	    if (chooser.showOpenDialog(chooser) == JFileChooser.APPROVE_OPTION) {
-
-	    	ambienteTrabalho =  String.valueOf(chooser.getSelectedFile())+"\\";
-
-	    	
-			this.confirma = JOptionPane.showConfirmDialog(null, "Confirma as configurações de Ambiente de Trabalho?\n\nDiretório do Ambiente de Trabalho:\n"+this.ambienteTrabalho+"\n\nCaso seja necessário configurar novamente, delete a pasta 'config'.", "Confimação das Configurações do Ambiente de Trabalho", JOptionPane.OK_CANCEL_OPTION);
-			
-			if (this.confirma == JOptionPane.CANCEL_OPTION) {
-
-				JOptionPane.showMessageDialog(null, "O Selenium nao foi configurado corretamente!\nO sistema será finalizado devido a falta de configuração.", "Configuração do Selenium", JOptionPane.ERROR_MESSAGE);
-				System.exit(0);
-				
-			}else {
-
-
-		    	//SalvaConfig gravarConfiguracao = new SalvaConfig(this.browserDriver, this.driver, this.ambienteTrabalho, this.indice);
-			
-			}
-
-
-	      }
-	    else {
-			JOptionPane.showMessageDialog(null, "O Ambiente de Trabalho não foi configurado corretamente!\nO sistema será finalizado devido a falta de configuração.", "Configuração do Ambiente de Trabalho", JOptionPane.ERROR_MESSAGE);
-			System.exit(0);
-	      }
-	}
 	
 }
 	
--- src/principal/SeniorTests.java
+++ src/principal/SeniorTests.java
@@ -8,7 +8,9 @@ import config.Cadastrar;;
 
 public class SeniorTests {
 	
-	private static final String projeto = "exemplo.jar";
+	
+	
+	private static String executavelJAR;
 	
 	private static String cenario;
 	
@@ -16,18 +18,23 @@ public class SeniorTests {
 	
 	public static void main (String args[]) {
 		
+		
+		while(true) {
+		
 		JOptionPane selecionarCenario = new JOptionPane();
+		
+		cenario = null;
 		
 		cenario = selecionarCenario.showInputDialog(null, "Insira o Cenário: ");
 		
-		if (cenario == null || cenario.length()<0){
+		if (cenario == null || cenario.equals("") || cenario.equals("sair") || cenario.equals("fechar")){
 			System.exit(0);
 			}else {
 				if (cenario.equals("config")) {
 					Cadastrar Configurar = new Cadastrar();
 					}else {
 						try {
-							Runtime.getRuntime().exec("java -jar "+projeto+" "+cenario);
+							Runtime.getRuntime().exec("java -jar "+executavelJAR+" "+cenario);
 							} catch (IOException e) {
 								// TODO Auto-generated catch block
 								e.printStackTrace();
@@ -35,4 +42,5 @@ public class SeniorTests {
 						}
 				}
 		}
+	}
 }
